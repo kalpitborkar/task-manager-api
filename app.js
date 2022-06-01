1d44901988626205bbc1a3c02b587990f596e122
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-06-01 16:23:12 +0530

    Setup and connected to database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -13,4 +15,14 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const PORT = 3000;
-app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
